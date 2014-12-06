28c52df61fd55f2231b920227b832a455c1bc470
Tobias Doll <tobias.doll@gmail.com>
2014-12-06 15:30:22 +0100

    Changed the statistics page: Typos, Reversed order of series, changed setting of series.

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -11,9 +11,21 @@ class StatisticsController < ApplicationController
     	@best_pace = Run.where(:UserID => current_user[:id]).order(pace_average: :asc)
     	@best_distance = Run.where(:UserID => current_user[:id]).order(distance: :desc)
     
-    	@last_distances = Run.where(:UserID => current_user[:id]).order(datetime: :desc).limit(10).pluck(:distance)
-    	@last_speeds = Run.where(:UserID => current_user[:id]).order(datetime: :desc).limit(10).pluck(:velocity_average)
-    	@last_paces = Run.where(:UserID => current_user[:id]).order(datetime: :desc).limit(10).pluck(:pace_average)
+        @last_dates = []
+        last_da = Run.where(:UserID => current_user[:id]).order(datetime: :desc).pluck(:datetime)
+        last_da.each do |la|
+            @last_dates.push la.strftime('%d.%m.%Y')
+        end
+        @last_dates.reverse!
+
+        @last_distances10 = []
+    	last_di = Run.where(:UserID => current_user[:id]).order(datetime: :desc).limit(10).pluck(:distance)
+        last_di.each do |di|
+            @last_distances10.push di/1000
+        end
+        @last_distances10.reverse!
+    	@last_speeds10 = Run.where(:UserID => current_user[:id]).order(datetime: :desc).limit(10).pluck(:velocity_average).reverse!
+    	@last_paces10 = Run.where(:UserID => current_user[:id]).order(datetime: :desc).limit(10).pluck(:pace_average).reverse!
 	end
 
 end
